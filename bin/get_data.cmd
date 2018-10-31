76d09058a5db1de2fd51841e4d1d0d58ea043750
xynshine <xynshine@yandex.ru>
2018-10-31 19:34:41 +0300

    add: copy scripts for dlls and data

---
+++ bin/get_data.cmd
@@ -0,0 +1,1 @@
+@xcopy /E /C /Y ..\data .\data
---
+++ bin/get_x64.cmd
@@ -0,0 +1,3 @@
+@xcopy /Y ..\3rdparty\glew-1.11.0\bin\Release\x64\glew32.dll
+@xcopy /Y ..\3rdparty\FreeImage\x64\FreeImage.dll
+@xcopy /Y ..\3rdparty\AntTweakBar\lib-build\AntTweakBar64.dll
