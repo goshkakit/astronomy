67d9eced8134b2819c5b249a6689e74736d15388
xynshine <xynshine@yandex.ru>
2018-10-31 19:19:11 +0300

    changed: EarthSpaceRenderer: exe->lib

--- src/EarthSpaceRenderer/es_view.cpp
+++ src/EarthSpaceRenderer/es_view.cpp
@@ -1,6 +1,7 @@
 #include "es_view.h"
 #include "es_globals.h"
-#include "main.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uiMsg, WPARAM wParam, LPARAM lParam);
 
 COpenGLView::COpenGLView()
 {
@@ -394,7 +395,7 @@ void COpenGLView::OnPaint()
 
 
 		vec3 pos2;
-		// прогноз положения в километрах
+		// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 		pos.x = eci1.Position().m_x;
 		pos.y = eci1.Position().m_y;
 		pos.z = eci1.Position().m_z;
@@ -450,4 +451,49 @@ void COpenGLView::OnSize(int Width, int Height)
 
 	// Send the new window size to AntTweakBar
 	TwWindowSize(Width, Height);
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uiMsg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		OpenGLView.OnKeyDown((UINT)wParam);
+		break;
+
+	case WM_LBUTTONDOWN:
+		OpenGLView.OnLButtonDown(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_MOUSEMOVE:
+		OpenGLView.OnMouseMove(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case 0x020A: // WM_MOUSWHEEL
+		OpenGLView.OnMouseWheel(HIWORD(wParam));
+		break;
+
+	case WM_PAINT:
+		OpenGLView.OnPaint();
+		break;
+
+	case WM_RBUTTONDOWN:
+		OpenGLView.OnRButtonDown(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_SIZE:
+		OpenGLView.OnSize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	default:
+		return DefWindowProc(hWnd, uiMsg, wParam, lParam);
+	}
+
+	return 0;
 }
